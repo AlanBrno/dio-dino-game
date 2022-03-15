84f136faca5758fb59d15f069b0c988f81aa08d3
AlanBrno <alanbr.no2@icloud.com>
2022-03-15 19:04:10 -0300

    Dio Dino Game

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.bg-dino');
+let isJumping = false;
+let position = 0; 
+
+function handleKeyUP(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping)
+        jump();
+    }
+}
+
+function gameOverEvent(event) {
+    if(event.keyCode === 13) {
+        location.reload();
+    }
+
+}
+
+function jump() {
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px'
+                }
+            }, 20);
+
+        } else {
+            isJumping = true;
+            position += 20;
+            dino.style.bottom = position + 'px'
+        }
+    }, 20);
+
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1240; 
+    let randomTime = Math.random() * 6000; 
+    
+    cactus.style.left = 1240 +'px';
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+        if( cactusPosition <- 60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if ( cactusPosition > 350 && cactusPosition < 410 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class ="game-over"> Fim de Jogo <br> Pressione Enter para Jogar Novamente. </h1>';
+            document.addEventListener('keyup', gameOverEvent)
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 10 );
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUP)
+
