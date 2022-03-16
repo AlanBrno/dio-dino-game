d5188c5438322361fbe29816f1791aa86ed88ead
AlanBrno <alanbr.no2@icloud.com>
2022-03-16 10:54:57 -0300

    Dio Dino Game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.bg-dino');
 let isJumping = false;
+let isGameOver = false;
 let position = 0; 
 
 function handleKeyUP(event) {
@@ -18,26 +19,28 @@ function gameOverEvent(event) {
 }
 
 function jump() {
-    let upInterval = setInterval(() => {
-        if (position >= 150) {
-            clearInterval(upInterval);
+     isJumping = true;
+        let upInterval = setInterval(() => {
+            if (position >= 150) {
 
-            let downInterval = setInterval(() => {
-                if (position <= 0) {
-                    clearInterval(downInterval);
-                    isJumping = false;
-                } else {
-                    position -= 20;
-                    dino.style.bottom = position + 'px'
-                }
-            }, 20);
+                clearInterval(upInterval);
 
-        } else {
-            isJumping = true;
-            position += 20;
-            dino.style.bottom = position + 'px'
-        }
-    }, 20);
+                let downInterval = setInterval(() => {
+                    if (position <= 0) {
+                        clearInterval(downInterval);
+                        isJumping = false;
+                    } else {
+                        position -= 20;
+                        dino.style.bottom = position + 'px'
+                    }
+                }, 20);
+
+            } else {
+                
+                position += 20;
+                dino.style.bottom = position + 'px'
+            }
+        }, 20);
 
 }
 
@@ -46,7 +49,9 @@ function createCactus() {
     let cactusPosition = 1240; 
     let randomTime = Math.random() * 6000; 
     
-    cactus.style.left = 1240 +'px';
+    if (isGameOver) return;
+
+    cactus.style.left = cactusPosition +'px';
     cactus.classList.add('cactus');
     background.appendChild(cactus);
 
@@ -54,16 +59,16 @@ function createCactus() {
 
         if( cactusPosition <- 60) {
             clearInterval(leftInterval);
-            background.removeChild(cactus);
-        } else if ( cactusPosition > 350 && cactusPosition < 410 && position < 60) {
-            clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class ="game-over"> Fim de Jogo <br> Pressione Enter para Jogar Novamente. </h1>';
-            document.addEventListener('keyup', gameOverEvent)
-        } else {
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
-        }
-    }, 10 );
+             background.removeChild(cactus);
+            } else if ( cactusPosition > 350 && cactusPosition < 410 && position < 60) {
+                clearInterval(leftInterval);
+                document.body.innerHTML = '<h1 class ="game-over"> Fim de Jogo <br> Pressione Enter para Jogar Novamente. </h1>';
+                document.addEventListener('keyup', gameOverEvent)
+            } else {
+                cactusPosition -= 10;
+                cactus.style.left = cactusPosition + 'px';
+            }
+        }, 10);
     setTimeout(createCactus, randomTime);
 }
 
